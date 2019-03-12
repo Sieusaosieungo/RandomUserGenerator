78955e15565d0caadcb70b24950a2cf9d4d1547c
Vu Duy Manh <manhdeptrai989@gmail.com>
2019-03-12 12:08:54 +0700

    Updates

--- precache-manifest.f79bee484a9301a6ecda00478f1d5ee5.js
+++ precache-manifest.f79bee484a9301a6ecda00478f1d5ee5.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/deployedclicky/static/js/runtime~main.d1e50f32.js"
   },
   {
-    "revision": "3ca08e3d24ebecdeb7cc",
-    "url": "/deployedclicky/static/js/main.a2883749.chunk.js"
+    "revision": "d5e9a0e9f6050047d8c6",
+    "url": "/deployedclicky/static/js/main.b6be592b.chunk.js"
   },
   {
     "revision": "910df776f0e356c36368",
     "url": "/deployedclicky/static/js/2.d51526cf.chunk.js"
   },
   {
-    "revision": "3ca08e3d24ebecdeb7cc",
+    "revision": "d5e9a0e9f6050047d8c6",
     "url": "/deployedclicky/static/css/main.d5d0c061.chunk.css"
   },
   {
-    "revision": "966511a7b2add82b10b69e3fc13aba21",
+    "revision": "e592b740ad2f6cb66a354750d5d8a913",
     "url": "/deployedclicky/index.html"
   }
 ];
