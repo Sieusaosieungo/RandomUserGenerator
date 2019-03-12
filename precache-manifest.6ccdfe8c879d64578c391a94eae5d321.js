f274b24eb30eab66432e9ccc8886ba4bf25562ad
Vu Duy Manh <manhdeptrai989@gmail.com>
2019-03-12 12:34:00 +0700

    Updates

---
+++ precache-manifest.6ccdfe8c879d64578c391a94eae5d321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "432140243b5a3e4f0d3fb7edd53548f0",
+    "url": "/randomusergenerator/static/media/text.43214024.png"
+  },
+  {
+    "revision": "ad90b9193680a4a619ee",
+    "url": "/randomusergenerator/static/js/runtime~main.35cfe928.js"
+  },
+  {
+    "revision": "ba7129c3e477d2dd6bc2",
+    "url": "/randomusergenerator/static/js/main.f10df4da.chunk.js"
+  },
+  {
+    "revision": "b022ad0beefb65bc8ba8",
+    "url": "/randomusergenerator/static/js/2.7a94a121.chunk.js"
+  },
+  {
+    "revision": "ba7129c3e477d2dd6bc2",
+    "url": "/randomusergenerator/static/css/main.f92b05cc.chunk.css"
+  },
+  {
+    "revision": "018150ce79937325ff122f6ec03aa5b1",
+    "url": "/randomusergenerator/index.html"
+  }
+];
