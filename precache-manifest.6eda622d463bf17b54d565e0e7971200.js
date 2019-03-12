0feec41a122c00d1baa5d290f112be0ecd4538ad
Vu Duy Manh <manhdeptrai989@gmail.com>
2019-03-12 12:04:46 +0700

    Updates

---
+++ precache-manifest.6eda622d463bf17b54d565e0e7971200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "432140243b5a3e4f0d3fb7edd53548f0",
+    "url": "/deployedclicky/static/media/text.43214024.png"
+  },
+  {
+    "revision": "d727bb44f84ac82a5b45",
+    "url": "/deployedclicky/static/js/runtime~main.d1e50f32.js"
+  },
+  {
+    "revision": "3ca08e3d24ebecdeb7cc",
+    "url": "/deployedclicky/static/js/main.a2883749.chunk.js"
+  },
+  {
+    "revision": "910df776f0e356c36368",
+    "url": "/deployedclicky/static/js/2.d51526cf.chunk.js"
+  },
+  {
+    "revision": "3ca08e3d24ebecdeb7cc",
+    "url": "/deployedclicky/static/css/main.d5d0c061.chunk.css"
+  },
+  {
+    "revision": "966511a7b2add82b10b69e3fc13aba21",
+    "url": "/deployedclicky/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab6e6173111bcd5d458e6671dc707d75.js"
+  "/deployedclicky/precache-manifest.6eda622d463bf17b54d565e0e7971200.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/deployedclicky/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
